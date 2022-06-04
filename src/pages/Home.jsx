85580e71eee43266b600c06a1e2fb16b649251de
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-04 20:49:51 +0300

    chore: convert Home function into class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,27 +2,35 @@ import React from "react";
 import HomeCategory from "../components/HomeCategory";
 import Layout from "../components/Layout";
 
-function Home(props) {
-  console.log(props);
+class Home extends React.Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
 
-  return (
-    <div>
-      <Layout>
-        <div className="container">
-          <div className="row">
-            <HomeCategory
-              image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-running-shoes-lead-1576249557.jpg"
-              title="Pantofi"
-              descrition="Cei mai tari pantofi"
-            />
-            <HomeCategory />
-            <HomeCategory />
-            <HomeCategory />
+  render(){
+    return (
+      <div>
+        <Layout>
+        
+          <div className="container">
+            <div className="row">
+              <HomeCategory
+                image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-running-shoes-lead-1576249557.jpg"
+                title="Pantofi"
+                descrition="Cei mai tari pantofi"
+              />
+              <HomeCategory/>
+              <HomeCategory/>
+              <HomeCategory/>
+            </div>
           </div>
-        </div>
-      </Layout>
-    </div>
-  );
+        </Layout> 
+      </div>
+    )
+  }
+
 }
+
 
 export default Home;
