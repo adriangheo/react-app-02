1085008e621bb50aa49bb90c3da33c39ffc4c646
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-04 20:01:30 +0300

    feat: add Layout component to About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Layout from '../components/Layout'
 
 function About() {
   return (
     <div>
+      <Layout>
         <h1>About</h1>
+        <p>Lorem ipsum</p>
+      </Layout>
     </div>
   )
 }
