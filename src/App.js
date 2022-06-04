07fe399f59ec05abef741aff9e5c92ce2bfa13db
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-04 19:51:27 +0300

    feat: implement page404

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home'
-import Login from './pages/Login';
-import About from './pages/About';
-import Page404 from './pages/Page404';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Page404 from "./pages/Page404";
 
-
-class App extends React.Component{
-  constructor(){
+class App extends React.Component {
+  constructor() {
     super();
     this.state = {};
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div>
-        React App 02
-        <Route path='/' exact component={Home} />
-        <Route path='/login' exact component={Login} />
-        <Route path='/about' exact component={About} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/about" exact component={About} />
+
+          <Route path="*" component={Page404} />
+        </Switch>
       </div>
-    )
+    );
   }
 }
 
