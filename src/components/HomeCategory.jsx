ecb4f898749fcca45db5dad2274fc1ea9d58eb36
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-04 20:44:47 +0300

    feat: add mock image for HomCategory.jsx

--- src/components/HomeCategory.jsx
+++ src/components/HomeCategory.jsx
@@ -3,6 +3,12 @@ import React from 'react'
 function HomeCategory() {
   return (
     <div className="col-6">
+        <img 
+            className='w-100 mb-2'
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-running-shoes-lead-1576249557.jpg" 
+            alt="shoes image" />
+        <h2>Pantofi</h2>
+        <p>Cei mai tari pantofi!</p>
         <h1>
             HomeCategory
         </h1>
