8e0930a59551bf8de7a44bdabedc5185231c5136
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-04 20:00:12 +0300

    feat: add Layout component to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/Layout";
 
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Layout>
+        <h1>Home</h1>
+      </Layout>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
