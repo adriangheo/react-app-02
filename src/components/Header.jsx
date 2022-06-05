2f15648cc1d2f3672476094f70d74ef06eff0a19
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-06 01:54:23 +0300

    feat: add css to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/images/logo.png'
+import './Header.css'
 
 function Header() {
   return (
-    <div>
+    <div className='header'>
         <Link to='/' >
           <img src={logo} alt="logo" />
         </Link> &nbsp;
