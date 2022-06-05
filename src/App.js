6dd15cee2e8dc06e88ab91f6d1dd0414f0bc5f2f
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-06 02:01:22 +0300

    feat: add category page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import About from "./pages/About";
-import Page404 from "./pages/Page404";
+import React from 'react';
+import { Route, Switch  } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
+import About from './pages/About.jsx';
+import Page404 from './pages/Page404.jsx';
+import Category from './pages/Category';
 
-class App extends React.Component {
-  constructor() {
+
+class App extends React.Component{
+  constructor(){
     super();
     this.state = {};
   }
 
-  render() {
-    return (
+  render(){
+    return(
       <div>
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/about" exact component={About} />
+          <Route path='/' exact component={Home} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/about' exact component={About} />
+          <Route path='/category' exact component={Category}/>
 
-          <Route path="*" component={Page404} />
+          <Route path='*' component={Page404} />
         </Switch>
       </div>
-    );
+    )
   }
 }
 
