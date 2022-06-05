ddb030820e29a626e25c029d1ad8af626f6a3d8f
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-06 02:02:21 +0300

    feat: add link to category page on home page img

--- src/components/HomeCategory.jsx
+++ src/components/HomeCategory.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function HomeCategory(props) {
   const {image, title, description} = props;
 
   return (
     <div className="col-6">
+      <Link to='/category'>
         <img 
-            className='w-100 mb-2'
-            src={image} 
-            alt='' />
-        <h2>{title}</h2>
-        <p>{description}</p>
+              className='w-100 mb-2'
+              src={image} 
+              alt='' />
+          <h2>{title}</h2>
+          <p>{description}</p>
+      </Link>
     </div>
   )
 }
