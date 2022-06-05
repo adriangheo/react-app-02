9d037c573636e529cb46cb47d2070091b605581c
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-06 02:05:22 +0300

    fix: change Category.jsx from fn to class, in order to use state

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Layout from '../components/Layout'
 
-function Category() {
-  return (
-    <div>
-        <Layout>
-            <h1>Category page</h1>
-        </Layout>
-    </div>
-  )
+
+export class Category extends Component {
+  render() {
+    return (
+      <div>
+      <Layout>
+          <h1>Category page</h1>
+      </Layout>
+  </div>
+    )
+  }
 }
 
 export default Category
