bae7a99719579414f9fb0474ff5b92d33edbc9b9
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-06 03:54:54 +0300

    feat: basic add-item-to-cart implementation with redux implementation

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { addToCart } from '../redux/actions/cart';
+
 
 function ProductItem(props) {
-  const {id, name, price, currency, image} = props;
+  const {id, name, price, currency, image, addToCartInjected} = props;
 
   return (
     <div className="col-4">
         <img src={image} alt="product" className='w-100'/>
         <p>{name}</p>
         <p>{price} {currency}</p>
+        <button 
+          className='btn btn-outline-dark'
+          onClick={()=>{
+            addToCartInjected({
+              product: {
+                name, //same as "name: name"
+                price,
+                currency,
+                image
+              }
+            })
+          }}
+        >
+           Adaugă în coș
+        </button>
     </div>
   )
 }
 
-export default ProductItem
+function mapDispatchToProps(dispatch){
+  return {
+    addToCartInjected: (payload) => dispatch(addToCart(payload))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(ProductItem) 
