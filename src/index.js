116bccba1e75422c39eb10cad305d719fe2e7d69
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-06 04:08:04 +0300

    feat: remove color and underline for <a> tags

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter} from 'react-router-dom';
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Switch  } from 'react-router-dom';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 import { Provider } from "react-redux";
 import store from "./redux/store";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
