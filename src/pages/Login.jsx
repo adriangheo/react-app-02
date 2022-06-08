3f694a450d6136c205925eb2f77f20454853540e
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-06-09 00:50:38 +0300

    implement redux with cart and user data -part02

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,31 +5,50 @@ import {connect} from 'react-redux';
 
 import { loginUser } from '../redux/actions/user'
 
-function Login(props) {
-  const { history, loginUser } = props;
-  
-  // function handleGoogleLogin(){
-  //   const googleLoginResponse = signInWithGoogle();
-  //   googleLoginResponse.then((response) => {
-  //       console.log(response);
-  //     history.push('/');
-  //   });
-  // }
 
-  return (
-    <div className='login-parent'>
-        <h1>Login</h1>
-        <Link to='/'>Home</Link>
-        <Link to='/register'>Register</Link>
-        <button onClick={loginUser()}>Google SignIn</button>
-    </div>
-  )
-}
-
-function mapDispatchToProps(dispatch){
-  return {
-    loginUser: () => { dispatch(loginUser()) }
+class Login extends React.Component {
+  componentDidUpdate(prevProps){
+    if(this.props.userData !== prevProps.userData){
+      this.props.history.push('/');
+    }
   }
+  
+  render(){
+    const { loginUser } = this.props;
+
+    return(
+    <div className='login-page'>
+      <Link to='/'>
+        Home
+      </Link>
+      <h1 className='h2'>Login</h1>
+      <p>Alege providerul cu care vrei să te loghezi:</p>
+      <button
+        classname="btn btn-outline-dark d-flex align-items-center"
+        onClick={() => loginUser()}
+      >
+        <span className='tex-nowrap'>Loghează-te cu Google</span>
+      </button>
+      <Link to='/register'>Register</Link>
+      <button onClick={loginUser()}>Google SignIn</button>
+    </div>) 
+  }
+
 }
 
-export default connect(null, mapDispatchToProps)(Login);
+  const mapStateToProps = (state) => {
+    return{
+      userData: state.user.data
+    }
+  }
+  
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      loginUser: () => { dispatch(loginUser()) }
+    }
+  }
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
